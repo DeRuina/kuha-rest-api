398d9c7a3dd22acc4eb735a2268c5bfc2d91a31f
Dean Ruina <dean.ruina@csc.fi>
2024-12-31 14:17:26 +0200

    setup basic db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,17 +3,36 @@ package main
 import (
 	"log"
 
+	"github.com/DeRuina/KUHA-REST-API/internal/db"
 	"github.com/DeRuina/KUHA-REST-API/internal/env"
+	"github.com/DeRuina/KUHA-REST-API/internal/store"
 )
 
 func main() {
 
 	cfg := config{
-		addr: env.GetString("URL", ":8080"),
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "localhost:5432"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &api{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()

68b4bf3110ca51aeebb87cc7c25640cdbfd0c733
Dean Ruina <dean.ruina@csc.fi>
2025-05-12 15:49:31 +0300

    getBool for redis plugin

--- internal/env/env.go
+++ internal/env/env.go
@@ -25,5 +25,18 @@ func GetInt(key string, defOption int) int {
 	}
 
 	return valInt
+}
 
+func GetBool(key string, fallback bool) bool {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	boolVal, err := strconv.ParseBool(val)
+	if err != nil {
+		return fallback
+	}
+
+	return boolVal
 }

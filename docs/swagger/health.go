aac1a457603372768b557424d8d868a8c85c4366
Dean Ruina <dean.ruina@csc.fi>
2025-07-18 11:23:39 +0300

    refactor: health endpoint swagger docs

--- docs/swagger/health.go
+++ docs/swagger/health.go
@@ -2,6 +2,17 @@ package swagger
 
 // Health
 type HealthStatusResponse struct {
-	Env    string `json:"env" example:"production"`
-	Status string `json:"status" example:"ok"`
+	API           string `json:"api" example:"ok"`
+	DBArchinisis  string `json:"db_archinisis" example:"ok"`
+	DBAuth        string `json:"db_auth" example:"ok"`
+	DBFIS         string `json:"db_fis" example:"ok"`
+	DBKAMK        string `json:"db_kamk" example:"ok"`
+	DBKlab        string `json:"db_klab" example:"ok"`
+	DBTietoevry   string `json:"db_tietoevry" example:"ok"`
+	DBUTV         string `json:"db_utv" example:"ok"`
+	Env           string `json:"env" example:"development"`
+	Goroutines    int    `json:"goroutines" example:"21"`
+	Redis         string `json:"redis" example:"ok"`
+	UptimeSeconds int64  `json:"uptime_seconds" example:"149039"`
+	Version       string `json:"version" example:"1.2.1"`
 }
